a368fddaf50d1e4849de360e40bc3312c74d5b84
nbcsc <noahbcaldwell+scgithub@gmail.com>
2025-03-24 19:38:32 -0500

    Initial Commit

---
+++ components/Header.jsx
@@ -0,0 +1,29 @@
+import headerStyles from './../src/css/styles';
+
+<div className="header" id="header">
+    <div id="headerLeft">
+
+    </div>
+    <div id="headerCenter">
+        <div>
+
+        </div>
+        <div style={headerStyles.spacer}>
+
+        </div>
+        <div id="nav">
+            <a href="#" className="navLink">
+
+            </a>
+            <a href="#about" className="navLink">
+                
+            </a>
+            <a href="#lambdaApiExample" className="navLink">
+                
+            </a>
+        </div>
+    </div>
+    <div id="headerRight">
+
+    </div>
+</div>
---
+++ src/css/styles.jsx
@@ -0,0 +1,22 @@
+const headerStyles = {
+    spacer: {
+        display: 'flex',
+        width: '20px',
+        height: '20px',
+    },
+    navLink: {
+        display: 'flex',
+        flexDirection: 'row',
+        color: 'white',
+        fontSize: '16px',
+        textDecoration: 'none',
+        fontFamily: 'sans-serif',
+    },
+    header: {
+        backgroundColor: 'black',
+        height: '70px',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+    },
+}
